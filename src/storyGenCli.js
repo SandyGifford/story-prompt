4fb827af0636ed72cf87d65046a7d75d0486fd70
Sanford Gifford <sanford.r.gifford@gmail.com>
2021-11-23 13:30:41 -0500

    TEMPLATE env var

--- src/storyGenCli.js
+++ src/storyGenCli.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const { commonPaperTemplateStoryGen } = require("./storyGen");
 
-const TEMPLATE = "One day Anna was walking her {{NUMBER}} {{UNIT_OF_MEASURE}} commute to {{PLACE}} and found a {{ADJECTIVE}} {{NOUN}} on the ground.";
-
 const args = process.argv.slice(2);
 
-const { DATA_PATH, MAX_STR } = process.env;
+const {
+	DATA_PATH,
+	MAX_STR,
+	TEMPLATE = "One day Anna was walking her {{NUMBER}} {{UNIT_OF_MEASURE}} commute to {{PLACE}} and found a {{ADJECTIVE}} {{NOUN}} on the ground."
+} = process.env;
+
 const options = {};
 if (DATA_PATH) options.dataPath = DATA_PATH;
 if (MAX_STR) options.dataPath = MAX_STR;
